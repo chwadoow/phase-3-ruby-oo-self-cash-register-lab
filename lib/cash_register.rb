3a61721ec55229e6eb1eb2b1652287eee08c78c8
moses ngige <moses32ngigz@gmail.com>
2022-08-19 18:06:28 +0300

    commit tree 81417a698d81007a5e98c4b15833d49615950b9a

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,31 @@
+require 'pry'
+class CashRegister 
+    attr_accessor :discount ,:title, :price , :quantity , :total
+    def initialize (total=0,discount=20)
+        @total = total
+        @discount = discount
+        
+    end
+    def total=(total)
+    @total = total
+    end
+    def total 
+        @total
+    end
+    def add_item (title,price,quantity=1)
+        @price = price
+        @title = title
+        @quantity = quantity
+        total_price= price * quantity
+        @total += total_price
+        
+    end
 
+    
+    def apply_discount
+        @price -= @discount
+        
+     
+    end
+
+end
